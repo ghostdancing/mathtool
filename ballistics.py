a9661b89fdc0ec235f8adc5beda45bb8e1bccf23
ghostdancing <110235342+ghostdancing@users.noreply.github.com>
2022-08-04 18:39:19 +0200

    Removed comment

--- ballistics.py
+++ ballistics.py
@@ -7,7 +7,6 @@ def trajectory(plot_x, velocity, distance):
     y = - CONST_GRAVITY  * (t*t) / 2
     return y
 
-#ip.plot_enabled = True
 inspector = ip.use_function(trajectory, {'velocity': 710.0, 'distance': 100.0}, 'Drop is $result meters')
 inspector.plot_enabled = True
 inspector.plot_max = 'distance'
